1c46e98a5af5b4867453b586fe3975b7b75402fa
Olivia Weiss <oliviaweiss@Olivias-MacBook-Pro.local>
2018-11-05 12:51:38 -0500

    basic example

--- coins.py
+++ coins.py
@@ -1,17 +1,20 @@
 import matplotlib
 import cv2
 import sys
-
-# from matplotlib import pyplot as plt
+import numpy as np
 
 img = cv2.imread('coins.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
+# noise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
 while True:
-    cv2.imshow("sup", thresh)
+    cv2.imshow("example", opening)
 
     key = cv2.waitKey(0)
     if key == 27: #esc key quits out
         cv2.destroyAllWindows()
-        sys.exit()
+        break
